45a04a466cc0f7a66205c5671271def9cf7d820a
CutieMath <cutie.math@protonmail.com>
2021-08-15 18:46:42 +1000

    testing main page

--- govapp/urls.py
+++ govapp/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from .views import index, graph, science_projects
 
-from .views import graph, science_projects
 
 urlpatterns = [
+    url('', index),
     url(r'^$', graph),
     url(r'^api/projects', science_projects, name='projects'),
 ]
